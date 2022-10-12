61841dd005a69ddb64cebb1ac5a779e55fa33ca4
oreofreakshake <yoosufyazak@gmail.com>
2022-10-13 01:57:21 +0500

    real

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-purple-900">
+  <nav class="container mx-auto text-center text-white p-7 text-2xl font-mono font-semibold">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</div>
+</template>
+
+<script>
+
+export default{
+  name:"Header",
+}
+</script>
+
+
---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="
+    w-full
+    h-screen
+    bg-gradient-to-r
+    from-pink-500
+    via-red-500
+    to-yellow-500
+    background-animate
+  ">
+    <h1 class="text-center text-4xl font-mono font-bold p-12 text-white">Hello!</h1>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name:"Home",
+
+}
+</script>
+
+<style>
+  body {
+    font-family: "Inter", sans-serif;
+  }
+
+  .background-animate {
+    background-size: 400%;
+
+    -webkit-animation: AnimationName 3s ease infinite;
+    -moz-animation: AnimationName 3s ease infinite;
+    animation: AnimationName 3s ease infinite;
+  }
+
+  @keyframes AnimationName {
+    0%,
+    100% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+  }
+</style>
