69334a54a90ccef4fedad2e940aaa53a5d1ce5eb
oreofreakshake <yoosufyazak@gmail.com>
2022-10-30 23:19:10 +0500

    added photo and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,30 @@
 <template>
+    
+    <div class="text-center py-6 flex flex-col items-center font-mono text-base">
+        <div class="space-x-8 text-2xl my-8 flex justify-center">
+        <a class="p-2 rounded-lg flex" href="https://github.com/oreofreakshake" target="_blank" rel="noreferrer" aria-label="GitHub">
+            <svg stroke="#64ffda" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="inline-block" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+            </svg>
+        </a>
 
-    <div class="text-center py-16 flex flex-col items-center font-mono text-base">
+        <a class="p-2 rounded-lg flex" href="https://twitter.com/0reofreakshake" target="_blank" rel="noreferrer" aria-label="Twitter">
+            <svg stroke="#64ffda" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="inline-block" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+            </svg>
+        </a>
+        <a class="p-2 rounded-lg flex" href="https://www.instagram.com/_yazak_" target="_blank" rel="noreferrer" aria-label="Instagram">
+            <svg stroke="#64ffda" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="inline-block" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+            </svg>
+        </a>
+
+    </div>
+
+
+
+
+
+
         <a class="w-max" href="https://github.com/oreofreakshake/portfolio" target="_blank" rel="noreferrer"><span class="text-white">{{"<a>"}} View website code {{"</a>"}}</span></a>
-        <span class="font-mono text-lightslate text-sm pt-2">Designed and built by Yazak</span>
+        <span class="font-mono text-lightslate text-sm pt-2 pb-4">Designed and built by Yazak</span>
     </div>
 </template>
 
