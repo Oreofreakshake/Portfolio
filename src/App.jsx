5807627c835c3267975a9ad5bf4ad153c48816ec
Yazak <yoosufyazak@gmail.com>
2023-07-28 12:39:19 +0500

    font added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import Home from "./components/Home";
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <>
+            <div className="h-screen bg-gradient-to-r from-gray-800 font-Ysabeau to-gray-900">
+                <Home />
+            </div>
+        </>
+    );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        fontFamily: {
+            Ysabeau: ["Ysabeau Office", "sans-serif"],
+        },
         extend: {},
     },
     plugins: [],
